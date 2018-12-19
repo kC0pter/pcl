770db4ac0bfbd3a50dbbd32d35792ac20a05dec0
Philipp Schloyer <Philipp.Schloyer@hs-augsburg.de>
2018-12-19 18:03:33 +0100

    added timing and example info print to harris3d and harris6d

--- examples/custom/example_keypoint_harris_6d.cpp
+++ examples/custom/example_keypoint_harris_6d.cpp
@@ -6,6 +6,8 @@
 #include <pcl/keypoints/harris_6d_custom.h>
 #include <pcl/features/normal_3d_omp.h>
 
+#include <time.h>
+
 using namespace std;
 using namespace pcl;
 using namespace pcl::io;
@@ -19,7 +21,6 @@ void estimateKeypoints(const PointCloud<PointXYZRGB>::Ptr &src,
 	HarrisKeypoint6DCustom<PointXYZRGB, PointXYZI> keypoints_est;
 
 	keypoints_est.setInputCloud(src);
-	keypoints_est.setRadiusSearch(1);
 	keypoints_est.setNormals(normals_src);
 	keypoints_est.compute(keypoints_src);
 }
@@ -37,10 +38,14 @@ void estimateNormals(const PointCloud<PointXYZRGB>::Ptr &src,
 /* ---[ */
 int main(int argc, char** argv)
 {
-	PointCloud<PointXYZRGB>::Ptr src;
+	PointCloud<PointXYZRGB>::Ptr src(new PointCloud<PointXYZRGB>);
 	PointCloud<PointXYZI>::Ptr keypoints_src(new PointCloud<PointXYZI>);
 	PointCloud<Normal>::Ptr normals_src(new PointCloud<Normal>);
 	std::vector<int> p_file_indices;
+
+	clock_t tStart;
+
+	print_info("Harris6D example");
 
 	// Parse the command line arguments for .pcd files
 	p_file_indices = parse_file_extension_argument(argc, argv, ".pcd");
@@ -53,21 +58,35 @@ int main(int argc, char** argv)
 
 	// Load the files
 	print_info("Loading %s as source ...\n", argv[p_file_indices[0]]);
-	src.reset(new PointCloud<PointXYZRGB>);
+	tStart = clock();
 	if (loadPCDFile(argv[p_file_indices[0]], *src) == -1)
 	{
 		print_error("Error reading the input file!\n");
 		return (-1);
 	}
 
+	print_info("Loaded %lu points as the source dataset.\n", src->size());
+	print_info("CPU Time taken: %.2fs\n\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+
+	tStart = clock();
 	estimateNormals(src, *normals_src);
 	print_info("Estimated %lu normals for the source dataset.\n", normals_src->points.size());
+	print_info("CPU Time taken: %.2fs\n\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
 
+	tStart = clock();
 	estimateKeypoints(src, normals_src, *keypoints_src);
 	print_info("Found %lu keypoints for the source dataset.\n", keypoints_src->points.size());
+	print_info("CPU Time taken: %.2fs\n\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
 
 	// Write it to disk
-	savePCDFileBinary("src_normals", *normals_src);
-	savePCDFileBinary("keypoints_src.pcd", *keypoints_src);
+	tStart = clock();
+	savePCDFileBinary("harris6d_src_normals", *normals_src);
+	print_info("Saved %lu normals to file.\n", normals_src->points.size());
+	print_info("CPU Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+
+	tStart = clock();
+	savePCDFileBinary("harris6d_keypoints_src.pcd", *keypoints_src);
+	print_info("Saved %lu keypoints to file.\n", keypoints_src->points.size());
+	print_info("CPU Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
 }
 /* ]--- */

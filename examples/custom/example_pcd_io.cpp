e3bf5969d8b588dcbe7fc12ded2ef96601975f94
Philipp Schloyer <Philipp.Schloyer@hs-augsburg.de>
2019-01-04 16:07:32 +0100

    added pcd to pcd binary functionality

--- examples/custom/example_pcd_io.cpp
+++ examples/custom/example_pcd_io.cpp
@@ -3,7 +3,6 @@
 #include <pcl/point_cloud.h>
 
 #include <pcl/io/pcd_io.h>
-#include <pcl/features/fpfh_omp.h>
 
 #include <time.h>
 
@@ -12,62 +11,121 @@ using namespace pcl;
 using namespace pcl::io;
 using namespace pcl::console;
 
-////////////////////////////////////////////////////////////////////////////////
-void estimateFPFH(const PointCloud<PointXYZ>::Ptr &src,
-	const PointCloud<Normal>::Ptr &normals_src,
-	const PointCloud<PointXYZ>::Ptr &keypoints_src,
-	PointCloud<FPFHSignature33> &fpfhs_src)
+void
+printHelp(int, char **argv)
 {
-	FPFHEstimationOMP<PointXYZ, Normal, FPFHSignature33> fpfh_est;
-	fpfh_est.setInputCloud(keypoints_src);
-	fpfh_est.setInputNormals(normals_src);
-	fpfh_est.setSearchSurface(src);
-	fpfh_est.compute(fpfhs_src);
+	std::cout << std::endl;
+	std::cout << "****************************************************************************" << std::endl;
+	std::cout << "*                                                                          *" << std::endl;
+	std::cout << "*                   PCD 2 PCD binary CONVERTER - Usage Guide               *" << std::endl;
+	std::cout << "*                                                                          *" << std::endl;
+	std::cout << "****************************************************************************" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Usage: " << argv[0] << " [Options] input.pcd output.pcd" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Options:" << std::endl;
+	std::cout << std::endl;
+	std::cout << "     --help   : Show this help" << std::endl; 
+	std::cout << "     --field  : Set the field to extract data from. Supported fields:" << std::endl;
+	std::cout << "                - normal" << std::endl;
+	std::cout << "                * xyz (default)" << std::endl;
+	std::cout << "                - rgb" << std::endl;
+	std::cout << "                - curvature" << std::endl;
+	std::cout << "                - intensity" << std::endl;
+	std::cout << std::endl;
+}
 
+bool
+loadCloud(const std::string &filename, pcl::PCLPointCloud2 &cloud)
+{
+	print_highlight("Loading "); print_value("%s ", filename.c_str());
+
+	if (loadPCDFile(filename, cloud) < 0)
+		return (false);
+	print_info("[done, "); print_value("%d", cloud.width * cloud.height); print_info(" points]\n");
+	print_info("Available dimensions: "); print_value("%s\n", pcl::getFieldsList(cloud).c_str());
+
+	return (true);
 }
 
 /* ---[ */
 int main(int argc, char** argv)
 {
-	PointCloud<PointXYZ>::Ptr src(new PointCloud<PointXYZ>);
-	PointCloud<PointXYZ>::Ptr keypoints_src(new PointCloud<PointXYZ>);
-	PointCloud<Normal>::Ptr normals_src(new PointCloud<Normal>);
-	PointCloud<FPFHSignature33>::Ptr fpfhs_src(new PointCloud<FPFHSignature33>);
-	std::vector<int> p_file_indices;
-
 	clock_t tStart;
 
-	print_info("FPFH example\n");
+	print_info("Convert a PCD file to PCD binary format.\nFor more information, use: %s --help\n", argv[0]);
 
-	// Parse the command line arguments for .pcd files
-	p_file_indices = parse_file_extension_argument(argc, argv, ".pcd");
-	if (p_file_indices.size() != 3)
+	if (argc < 3 || pcl::console::find_switch(argc, argv, "--help"))
 	{
-		print_error("Need three input PCD file to continue.\n");
-		print_error("Order is: source.pcd keypoints_src.pcd normals_src.pcd\n");
+		printHelp(argc, argv);
 		return (-1);
 	}
 
-	// Load the files
-	print_info("Loading %s as source, %s as keypoints_src and %s as normals_src ...\n", argv[p_file_indices[0]], argv[p_file_indices[1]], argv[p_file_indices[2]]);
-	tStart = clock();
-	if ((loadPCDFile(argv[p_file_indices[0]], *src) == -1) && 
-		(loadPCDFile(argv[p_file_indices[1]], *keypoints_src) == -1) && 
-		(loadPCDFile(argv[p_file_indices[2]], *normals_src) == -1))
+	// Parse the command line arguments for .pcd and .png files
+	std::vector<int> pcd_file_index = parse_file_extension_argument(argc, argv, ".pcd");
+
+	if (pcd_file_index.size() != 2)
 	{
-		print_error("Error reading the input files!\n");
+		print_error("Need one input PCD file and one output PCD file.\n");
 		return (-1);
 	}
 
-	print_info("Loaded %lu points as the source dataset.\n", src->size());
-	print_info("	   %lu points as the keypoints dataset.\n", keypoints_src->size());
-	print_info("	   %lu vectors as the normals dataset.\n", normals_src->size());
-	print_info("CPU Time taken: %.2fs\n\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+	std::string pcd_filename_input = argv[pcd_file_index[0]];
+	std::string pcd_filename_output = argv[pcd_file_index[1]];
 
-	// Write it to disk
-	tStart = clock();
-	savePCDFileBinary("fpfh_src", *fpfhs_src);
-	print_info("Saved %lu fpfh features to file.\n", fpfhs_src->points.size());
-	print_info("CPU Time taken: %.2fs\n\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+	// Load the input file
+	pcl::PCLPointCloud2::Ptr blob(new pcl::PCLPointCloud2);
+
+	print_info("Loading %s as input file ...\n", pcd_filename_input);
+	if (!loadCloud(pcd_filename_input, *blob))
+	{
+		print_error("Unable to load PCD file.\n");
+		return (-1);
+	}
+
+	std::string field_name = "xyz";
+	parse_argument(argc, argv, "--field", field_name);
+	print_info("Field name: "); print_value("%s\n", field_name.c_str());
+
+	if (field_name == "normal")
+	{
+		PointCloud<PointNormal> cloud;
+		fromPCLPointCloud2(*blob, cloud);
+		savePCDFileBinary(pcd_filename_output, cloud);
+		print_info("Saved %lu points to file.\n", cloud.points.size());
+	}
+	else if (field_name == "rgb")
+	{
+		PointCloud<PointXYZRGB> cloud;
+		fromPCLPointCloud2(*blob, cloud);
+		savePCDFileBinary(pcd_filename_output, cloud);
+		print_info("Saved %lu points to file.\n", cloud.points.size());
+	}
+	else if (field_name == "xyz")
+	{
+		PointCloud<PointXYZ> cloud;
+		fromPCLPointCloud2(*blob, cloud);
+		savePCDFileBinary(pcd_filename_output, cloud);
+		print_info("Saved %lu points to file.\n", cloud.points.size());
+	}
+	else if (field_name == "curvature")
+	{
+		PointCloud<PointNormal> cloud;
+		fromPCLPointCloud2(*blob, cloud);
+		savePCDFileBinary(pcd_filename_output, cloud);
+		print_info("Saved %lu points to file.\n", cloud.points.size());
+	}
+	else if (field_name == "intensity")
+	{
+		PointCloud<PointXYZI> cloud;
+		fromPCLPointCloud2(*blob, cloud);
+		savePCDFileBinary(pcd_filename_output, cloud);
+		print_info("Saved %lu points to file.\n", cloud.points.size());
+	}
+	else
+	{
+		print_error("Unsupported field \"%s\".\n", field_name.c_str());
+		return (-1);
+	}
 }
 /* ]--- */
